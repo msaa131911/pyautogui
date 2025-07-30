d6ed063f949d3921011f382a5144f6104f84d86d
MSAA <msaa131911@gmail.com>
2025-07-30 15:13:40 +0600

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,17 @@
+import pyautogui as at
+import time
+
+time.sleep(5)
+
+
+#print(at.position())
+
+
+#at.mouseDown()
+#at.moveTo(497,226,2)
+#at.mouseUp()
+
+#at.dragTo(497, 226, 2)
+#at.scroll(10)
+
+
